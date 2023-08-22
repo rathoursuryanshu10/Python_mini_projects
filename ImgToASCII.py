6d88896fbb4971bbdf2d942619bcdbb76c819ac0
suryansh Rathour <66956879+rathoursuryanshu10@users.noreply.github.com>
2023-08-23 00:06:00 +0530

    Create ImgToASCII.py

---
+++ ImgToASCII.py
@@ -0,0 +1,8 @@
+import pywhatkit as kit
+
+kit.image_to_ascii_art("img2.png","ascii.txt")
+"""
+import ascii_magic
+output=ascii_magic.from_image_file("img2.png",columns=300,char='#')
+ascii_magic.terminal(output)
+"""
