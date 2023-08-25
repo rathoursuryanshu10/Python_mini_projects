b1cc26d866e1cbff56d449742b538d20dac07cec
suryansh Rathour <66956879+rathoursuryanshu10@users.noreply.github.com>
2023-08-25 23:47:04 +0530

    Create pass_wordlists.py

---
+++ pass_wordlists.py
@@ -0,0 +1,10 @@
+from string import *
+from itertools import product
+value=ascii_letters + digits + punctuation
+print(value)
+for i in range(8,16):
+    for j in product(value,repeat=i):
+        word="".join(j)
+        p=open("password.txt","a")
+        p.write(word)
+        p.write("\n")
