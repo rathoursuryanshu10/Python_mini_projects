b0b32d4ae118990257f39e603efbbd299d74c71d
suryansh Rathour <66956879+rathoursuryanshu10@users.noreply.github.com>
2023-09-14 00:32:20 +0530

    Create Stock_market_plot.py

---
+++ Stock_market_plot.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+ticker=input("Enter stock Name:: ")
+data=yf.download(ticker,start="2021-01-01",end="2023-03-03")
+plt.figure(figsize=(10,5))
+plt.plot(data["Close"])
+plt.plot(data["Close"])
+plt.title(f"{ticker} Stock Chart ")
+plt.xlabel("Date")
+plt.ylabel("Price (INR)")
+plt.show()
